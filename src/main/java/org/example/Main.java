3f68a587ec2922838a4dddd23e429df293007758
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-13 10:31:13 +0530

    Version 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        System.out.println("Hi This Is Version 1");
+        IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
