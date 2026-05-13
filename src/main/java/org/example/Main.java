be3cefa01e85341c2c07d2a4521fbb51fc10a6de
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-13 11:41:43 +0530

    Version 1 Restored

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     static void main() {
-        System.out.println("Hi This Is Version 2");
+        System.out.println("Hi This Is Version 1");
         IO.println(String.format("Hello and welcome!"));
 
         for (int i = 1; i <= 5; i++) {
